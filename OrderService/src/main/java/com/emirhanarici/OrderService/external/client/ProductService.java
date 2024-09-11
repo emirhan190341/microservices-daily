7bc59ab3e0c8e0dc41ca1c1f0ae54e8497970342
emirhanaricii <emirhan190341@gmail.com>
2024-09-11 14:18:13 +0300

    Implemented rate limiter with using resilience4j and redis

--- OrderService/src/main/java/com/emirhanarici/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/emirhanarici/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.emirhanarici.OrderService.external.client;
 
+import com.emirhanarici.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -14,5 +17,11 @@ public interface ProductService {
             @PathVariable("id") long productId,
             @RequestParam long quantity
     );
+
+    default ResponseEntity<Void> fallback(Exception e) {
+        throw new CustomException("Product Service is not available",
+                "UNAVAILABLE",
+                500);
+    }
 
 }
